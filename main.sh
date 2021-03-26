0120355c6efa77f06f7d9e30dbdcbe8fa4cca1b1
aliquanquan <1114807608@qq.com>
2021-03-26 20:24:37 +0800

    def user & passwd

--- main.sh
+++ main.sh
@@ -4,4 +4,10 @@ read name
 echo "您输入的用户名为：$name"
 echo "请输入密码："
 read passwd
-echo "您的密码是：$passwd"
+if [ "$name" = "user" -a "$passwd" = "1234567" ]
+then
+	echo "你的用户名为：$name"
+	echo "您的密码是：$passwd"
+else
+	echo 用户名或密码错误
+fi
